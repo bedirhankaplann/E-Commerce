ec8d825d68ad78e2aab9b015d5ca0b6edb46505e
bedirhankaplann <bedirhankaplann@users.noreply.github.com>
2019-10-19 22:36:24 +0300

    Upload v4.0 - Kategori

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -1,23 +1,14 @@
 <?php
     namespace App\Http\Controllers;
     use Illuminate\Routing\Controller;
-
+    use App\Models\Kategori;
     class HomepageController extends Controller
     {
         public function index()
         {
-            $name = "Bedirhan";
-            $surname = "Kaplan";
-            $names = ['Bedirhan', 'Sümeyye', 'Emre', 'Berna'];
-            $users = [
-                ['id' => 1, 'userName' => 'Bedirhan'],
-                ['id' => 2, 'userName' => 'Sümeyye'],
-                ['id' => 3, 'userName' => 'Emre'],
-                ['id' => 4, 'userName' => 'Berna'],
-            ];
-            return view('homepage', compact('name','surname', 'names'));
-            //return view('homepage')->with('name','Deneme'));
-            //return view('homepage')->with(['name' => $isim], ['surname' => $surname ]));
+            $kategoriler = Kategori::all()->take(7);
+            return view('homepage', compact('kategoriler'));
+
         }
     }
 ?>
--- public/js/app.js
+++ public/js/app.js
@@ -29921,8 +29921,8 @@ if (token) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\projects\ecommerce\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\projects\ecommerce\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/code/ecommerce/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/code/ecommerce/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
